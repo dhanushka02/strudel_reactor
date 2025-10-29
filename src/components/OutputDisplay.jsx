a18e498077947bf3a6eb53c543768a6d1aaeb2d6
Dhanushka Lakshan <dhanushkalakshan123abc@gmail.com>
2025-10-30 10:13:01 +1030

    Added Basic content for OutputDisplay.jsx (created the card, title and seperate area for display output)

---
+++ src/components/OutputDisplay.jsx
@@ -0,0 +1,17 @@
+// src/components/OutputDisplay.jsx
+
+export default function OutputDisplay() {
+    return (
+        <div className="card p-3 mb-3">
+            <div className="d-flex justify-content-between align-items-center mb-2">
+                <h6 className="fw-bold text-dark">Strudel REPL Output</h6>
+                <span className="badge bg-light text-dark border">Stopped</span>
+            </div>
+
+            {/* REPL Output Area */}
+            <div id="editor" />
+
+            
+        </div>
+    );
+}
