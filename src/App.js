0ee087ebc75569ca59a56be7e172dc074317ae73
Dhanushka Lakshan <dhanushkalakshan123abc@gmail.com>
2025-10-29 16:26:10 +1030

    Created seperate .jsx file for Navbar, Preprocessor Editor, Output Display and D3Visualizer and import them to App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import { initAudioOnFirstClick } from '@strudel/webaudio';
 import { transpiler } from '@strudel/transpiler';
 import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/webaudio';
 import { registerSoundfonts } from '@strudel/soundfonts';
-import { stranger_tune } from './tunes';
+import { stranger_tune } from './assets/tunes';
 import console_monkey_patch, { getD3Data } from './console-monkey-patch';
+
+import "./styles/layout.css";
+
+import NavbarControls from './components/NavbarControls';
+import PreprocessorEditor from './components/PreprocessorEditor';
+import OutputDisplay from './components/OutputDisplay';
+import D3Visualizer from './components/D3Visualizer';
+import ControlsPanel from './components/ControlsPanel';
 
 let globalEditor = null;
 
@@ -107,51 +115,76 @@ useEffect(() => {
 
 
 return (
-    <div>
-        <h2>Strudel Demo</h2>
-        <main>
+    <div className='App'>
+        {/* Top Navbar */
+        <NavbarControls />}
 
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-                        <textarea className="form-control" rows="15" id="proc" ></textarea>
-                    </div>
-                    <div className="col-md-4">
-
-                        <nav>
-                            <button id="process" className="btn btn-outline-primary">Preprocess</button>
-                            <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
-                            <br />
-                            <button id="play" className="btn btn-outline-primary">Play</button>
-                            <button id="stop" className="btn btn-outline-primary">Stop</button>
-                        </nav>
-                    </div>
+        {/* Main Content */}
+        <div className="container-fluid px-4 py-3">
+            <div className='row gx-3 gy-3'>
+                {/* Left Column */}
+                <div className='col-lg-4 col-md-6'>
+                    <PreprocessorEditor />
                 </div>
-                <div className="row">
-                    <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <div id="editor" />
-                        <div id="output" />
-                    </div>
-                    <div className="col-md-4">
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
-                            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                p1: ON
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                p1: HUSH
-                            </label>
-                        </div>
-                    </div>
+                
+                {/* Middle Column */}
+                <div className='col-lg-8 col-md-6'>
+                    <OutputDisplay />
+                    <D3Visualizer />
+                </div>
+                {/* Right Column */}
+                <div className='col-lg-4 col-md-12'> 
+                    <ControlsPanel />
                 </div>
             </div>
-            <canvas id="roll"></canvas>
-        </main >
-    </div >
+        </div>
+
+    </div>
+    // <div>
+    //     <h2>Strudel Demo</h2>
+    //     <main>
+
+    //         <div className="container-fluid">
+    //             <div className="row">
+    //                 <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+    //                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
+    //                     <textarea className="form-control" rows="15" id="proc" ></textarea>
+    //                 </div>
+    //                 <div className="col-md-4">
+
+    //                     <nav>
+    //                         <button id="process" className="btn btn-outline-primary">Preprocess</button>
+    //                         <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+    //                         <br />
+    //                         <button id="play" className="btn btn-outline-primary">Play</button>
+    //                         <button id="stop" className="btn btn-outline-primary">Stop</button>
+    //                     </nav>
+    //                 </div>
+    //             </div>
+    //             <div className="row">
+    //                 <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+    //                     <div id="editor" />
+    //                     <div id="output" />
+    //                 </div>
+    //                 <div className="col-md-4">
+    //                     <div className="form-check">
+    //                         <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
+    //                         <label className="form-check-label" htmlFor="flexRadioDefault1">
+    //                             p1: ON
+    //                         </label>
+    //                     </div>
+    //                     <div className="form-check">
+    //                         <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
+    //                         <label className="form-check-label" htmlFor="flexRadioDefault2">
+    //                             p1: HUSH
+    //                         </label>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         <canvas id="roll"></canvas>
+    //     </main >
+    // </div >
 );
 
 
