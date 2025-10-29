d9d02c15eced870a2401f5bfc9094681d505a973
Dhanushka Lakshan <dhanushkalakshan123abc@gmail.com>
2025-10-29 17:23:21 +1030

    Added the basic content to preprocessorEditor.jsx

---
+++ src/components/PreprocessorEditor.jsx
@@ -0,0 +1,19 @@
+// src/components/PreprocessorEditor.jsx
+
+export default function PreprocessorEditor() {
+    return (
+        <div className="card p-3 h-100">
+            <div className="d-flex justify-content-between align-items-center mb-2">
+                <h6 className="f2-bold text-dark">Preprocessor Editor</h6>
+                <small className="text-muted">Edit your preprocessing code here</small>
+            </div>
+            <label htmlFor="proc" className="form-label">Text to preprocess</label>
+            <textarea
+                id="proc"
+                className="form-control"
+                rows={15}
+                placeholder="Paste or edit the Strudel code here...."
+            />
+        </div>
+    );
+}
