a6a00804c162181d330a9542cf384e00c4759288
Dhanushka Lakshan <dhanushkalakshan123abc@gmail.com>
2025-10-30 10:18:10 +1030

    Added basic content for D3Visualizer.jsx

---
+++ src/components/D3Visualizer.jsx
@@ -0,0 +1,10 @@
+// src/components/D3Visualizer.jsx
+
+export default function D3Visualizer() {
+    return(
+        <div className="card p-3">
+            <h6 className="fw-bold text-dark mb-2">Live Graph (D3)</h6>
+            <div className="text-muted">This graph visualizes the output of the Strudel REPL in real-time.</div>
+        </div>
+    );
+}
